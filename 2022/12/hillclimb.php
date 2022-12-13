791fff3c2a4ff1fdf00d8e2df0db03100d757e52
Loren Lang <llang@asbury.edu>
2022-12-13 17:12:59 -0500

    2022 - Day 12 - started

--- 2022/12/hillclimb.php
+++ 2022/12/hillclimb.php
@@ -10,11 +10,36 @@ $data = new FileReader(currentDir('test.txt'));
 // $data = new FileReader(currentDir('data.txt'));
 
 $grid = [];
+$row  = 0;
 
 foreach ($data->rows() as $dataRow) {
 
-
+    foreach (str_split($dataRow) as $index => $char) {
+        $grid[$row][$index] = $char;
+        if ($char == 'S') {
+            $start = "$row|$index";
+        }
+        if ($char == 'E') {
+            $end = "$row|$index";
+        }
+    }
+    $row++;
 
 }
-
+draw($grid);
 // output("Total: $total");
+output("Start: $start");
+output("  End: $end");
+
+
+function draw($grid)
+{
+    for ($i = 0; $i < count($grid); $i++) {
+        for ($j = 0; $j < count($grid[$i]); $j++) {
+            echo $grid[$i][$j];
+        }
+        echo PHP_EOL;
+    }
+    echo PHP_EOL;
+    echo PHP_EOL;
+}
