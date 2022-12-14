f768fa80d7c832744f8e1ad0988a5b72f4e303a8
Loren Lang <loren@lorenlang.com>
2022-12-14 13:03:39 -0500

    2022 - Day 12

--- 2022/12/hillclimb.php
+++ 2022/12/hillclimb.php
@@ -6,40 +6,86 @@ require_once '../../autoload.php';
 require_once '../../functions.php';
 require_once '../../utility/FileReader.php';
 
-$data = new FileReader(currentDir('test.txt'));
-// $data = new FileReader(currentDir('data.txt'));
+// $data = new FileReader(currentDir('test.txt'));
+$data = new FileReader(currentDir('data.txt'));
 
-$grid = [];
-$row  = 0;
+
+$heights = array_merge(['S'], range('a', 'z'), ['E']);
+$grid    = [];
+$row     = 0;
 
 foreach ($data->rows() as $dataRow) {
 
     foreach (str_split($dataRow) as $index => $char) {
         $grid[$row][$index] = $char;
         if ($char == 'S') {
-            $start = "$row|$index";
+            $start = [$row, $index];
         }
         if ($char == 'E') {
-            $end = "$row|$index";
+            $dest = [$row, $index];
         }
     }
     $row++;
 
 }
-draw($grid);
-// output("Total: $total");
-output("Start: $start");
-output("  End: $end");
 
+define('MAX_X', count($grid) - 1);
+define('MAX_Y', count($grid[0]) - 1);
 
-function draw($grid)
-{
-    for ($i = 0; $i < count($grid); $i++) {
-        for ($j = 0; $j < count($grid[$i]); $j++) {
-            echo $grid[$i][$j];
-        }
-        echo PHP_EOL;
+$queue   = [makeGridKey($start) => 0];
+$visited = [$start];
+
+while ( ! empty($queue)) {
+    [$x, $y] = explode(',', array_key_first($queue));
+
+    $distance = array_shift($queue);
+
+    if ([$x, $y] == $dest) {
+        output(PHP_EOL);
+        dashline();
+        output("Destination found!");
+        output("Distance: $distance");
+        dashline();
+        output(PHP_EOL);
+        die();
     }
-    echo PHP_EOL;
-    echo PHP_EOL;
+
+    foreach (
+        [
+            [$x - 1, $y],
+            [$x + 1, $y],
+            [$x, $y - 1],
+            [$x, $y + 1],
+        ] as $coords
+    ) {
+
+        $gridKey = makeGridKey($coords);
+        if ($gridKey && ( ! visited($coords, $visited)) && isReachable(array_search($grid[$x][$y], $heights), $coords,
+                                                                       $grid, $heights)) {
+            $queue[$gridKey] = $distance + 1;
+            $visited[]       = $coords;
+        }
+    }
+
+}
+
+
+function makeGridKey(array $coords)
+{
+    return ($coords[0] < 0 || $coords[1] < 0 || $coords[0] > MAX_X || $coords[1] > MAX_Y) ? FALSE : join(',', $coords);
+}
+
+
+function visited(array $coords, array &$visited)
+{
+    return in_array($coords, $visited);
+}
+
+
+function isReachable(int $current, array $coords, array &$grid, array &$heights)
+{
+    $x = $coords[0];
+    $y = $coords[1];
+
+    return (isset($grid[$x][$y]) && (array_search($grid[$x][$y], $heights) - $current <= 1));
 }
