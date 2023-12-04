3c47c86c894a307196ffd0b85999d4daa9255d0c
llang <llang@asbury.edu>
2023-12-04 14:26:31 -0500

    2016 - Day 19 (in progress)

---
+++ 2016/19/gifts.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once '../../autoload.php';
+require_once '../../functions.php';
+
+require_once 'CircularLinkedList.php';
+
+
+//$numElves = 5;
+$numElves = 3017957;
+
+$elves = new ElfList($numElves);
+
+$current = $elves->find(1);
+
+while ($elves->count() > 1) {
+    $current->data['gifts'] += $current->next->data['gifts'];
+    $elves->deleteNode($current->next->data);
+    $current = $current->next;
+    output('Remaining: ' . $elves->count());
+}
+
+var_export($current->data);
+
+/*
+
+
+cll = CircularLinkedList()
+
+last = cll.addToEmpty(6)
+last = cll.addEnd(8)
+last = cll.addFront(2)
+last = cll.addAfter(10, 2)
+
+cll.traverse()
+
+last = cll.deleteNode(last, 8)
+print()
+cll.traverse()
+
+
+
+$cll = new CircularLinkedList();
+
+$last = $cll->addToEmpty(6);
+$last = $cll->addEnd(8);
+$last = $cll->addFront(2);
+$last = $cll->addAfter(10, 2);
+
+$cll->traverse();
+
+$last = $cll->deleteNode(10);
+echo PHP_EOL;
+$cll->traverse();
+ */
